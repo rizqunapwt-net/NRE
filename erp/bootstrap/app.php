793f8e7fc43c7838fe19ccf7b4affc1937ffd1ed
Tholib <tholibadilmaruf.campus@gmail.com>
2026-02-17 18:14:25 +0700

    chore: fix branding consistency and add trustProxies for production

--- erp/bootstrap/app.php
+++ erp/bootstrap/app.php
@@ -20,6 +20,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'permission' => PermissionMiddleware::class,
